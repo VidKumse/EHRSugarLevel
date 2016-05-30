bfb3af8d98597eb64d2051cfb1b6b099e8b19ed0
Vid Kumse <vid.kumse@gmail.com>
2016-05-30 16:04:57 +0200

    Uspešna povezava z GATT in zaznava servisov

--- app/src/main/java/com/example/vid/ble_test/Obrazec.java
+++ app/src/main/java/com/example/vid/ble_test/Obrazec.java
@@ -3,17 +3,60 @@ package com.example.vid.ble_test;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothManager;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.widget.SimpleExpandableListAdapter;
 import android.widget.TextView;
 
 public class Obrazec extends AppCompatActivity {
 
     TextView textView2;
     BluetoothDevice device;
+    private String mDeviceAddress;
+    private BLEService mBluetoothLeService;
+    private final static String TAG = Obrazec.class.getSimpleName();
+    private boolean mConnected = false;
+
+
+    //metoda za kontrolo življenjskega cikla servisa
+    private final ServiceConnection mServiceConnection = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder service) {
+            mBluetoothLeService = ((BLEService.LocalBinder) service).getService();
+            if (!mBluetoothLeService.initialize()) {
+                Log.e(TAG, "Ni useplo inicializirati bluetootha");
+                finish();
+            }
+            // Automatically connects to the device upon successful start-up initialization.
+            mBluetoothLeService.connect(mDeviceAddress);
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            mBluetoothLeService = null;
+        }
+    };
+
+    //ta del pridobi podatke od broadcastrecieverja, torej podatke o servisih
+    private final BroadcastReceiver mGattUpdateReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            final String action = intent.getAction();
+            System.out.println("bbbbbbbb"+action);
+        }
+    };
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +68,45 @@ public class Obrazec extends AppCompatActivity {
         textView2 = (TextView) findViewById(R.id.textView2);
         device = getIntent().getExtras().getParcelable("device");
         textView2.setText("Ime naprave: " + device.getName());
+
+        mDeviceAddress = device.getAddress();
+
+        //Kličemo service
+        Intent gattServiceIntent = new Intent(this, BLEService.class);
+        bindService(gattServiceIntent, mServiceConnection, BIND_AUTO_CREATE);
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(mGattUpdateReceiver, makeGattUpdateIntentFilter());
+        if (mBluetoothLeService != null) {
+            final boolean result = mBluetoothLeService.connect(mDeviceAddress);
+            Log.d(TAG, "Connect request result=" + result);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(mGattUpdateReceiver);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unbindService(mServiceConnection);
+        mBluetoothLeService = null;
+    }
+
+    private static IntentFilter makeGattUpdateIntentFilter() {
+        final IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(BLEService.ACTION_GATT_CONNECTED);
+        intentFilter.addAction(BLEService.ACTION_GATT_DISCONNECTED);
+        intentFilter.addAction(BLEService.ACTION_GATT_SERVICES_DISCOVERED);
+        intentFilter.addAction(BLEService.ACTION_DATA_AVAILABLE);
+        return intentFilter;
+    }
+
+
 }
